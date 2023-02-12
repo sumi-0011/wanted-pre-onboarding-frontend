0b65b08ba760c0b06bea9178c72ec37f88dc9ca8
sumi-0011 <selina2000@naver.com>
2023-02-12 23:41:50 +0900

    feat: sign up page routing setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import SignUp from './pages/signup';
+
 function App(): JSX.Element {
 	return (
 		<div className="App">
-			<header />
+			<BrowserRouter>
+				<header>
+					<Link to="/signup">
+						<li>회원가입</li>
+					</Link>
+				</header>
+				<Routes>
+					<Route path="/signup" element={<SignUp />} />
+				</Routes>
+			</BrowserRouter>
 		</div>
 	);
 }
